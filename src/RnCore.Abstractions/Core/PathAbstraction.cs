77e4e1b9a475a0eeb594584bf0171694fb806b5e
Richard Niemand <niemand.richard@gmail.com>
2023-01-12 10:15:06 -0700

    Adding Path.GetFileName()

--- src/RnCore.Abstractions/Core/PathAbstraction.cs
+++ src/RnCore.Abstractions/Core/PathAbstraction.cs
@@ -6,6 +6,7 @@ public interface IPathAbstraction
 {
   string GetFullPath(string path);
   string GetFullPath(string path, string basePath);
+  string? GetFileName(string? path);
   ReadOnlySpan<char> GetFileNameWithoutExtension(ReadOnlySpan<char> path);
   string? GetFileNameWithoutExtension(string? path);
   string Combine(params string[] paths);
@@ -17,6 +18,7 @@ public class PathAbstraction : IPathAbstraction
 {
   public string GetFullPath(string path) => Path.GetFullPath(path);
   public string GetFullPath(string path, string basePath) => Path.GetFullPath(path, basePath);
+  public string? GetFileName(string? path) => Path.GetFileName(path);
   public ReadOnlySpan<char> GetFileNameWithoutExtension(ReadOnlySpan<char> path) => Path.GetFileNameWithoutExtension(path);
   public string? GetFileNameWithoutExtension(string? path) => Path.GetFileNameWithoutExtension(path);
   public string Combine(params string[] paths) => Path.Combine(paths);
